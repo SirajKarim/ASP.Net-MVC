ddb969f123ce1750b24f89036de5f402428bfe6d
Muhammad Siraj <msirajakarim437@gmail.com>
2020-03-27 18:10:20 -0700

    email sending

---
+++ Email Esnding/Email Esnding/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Email_Esnding.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RegistrationPage()
+        {
+            return View();
+        }
+        public ActionResult ThankyouPage()
+        {
+            return View();
+        }
+
+    }
+}
